69fb33330407712f6c866651c861f9234e02603d
radu costache <sir.raddu@gmail.com>
2020-05-15 01:08:26 +0300

    first commit

---
+++ precache-manifest.4ebc288e55856606cd94c1075d9e8070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3aa07d47a2c5e34ba74c31ebfbf3d0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae306707f3a1edab1127",
+    "url": "/static/css/main.dee5888c.chunk.css"
+  },
+  {
+    "revision": "c02b3f0386435daf48dc",
+    "url": "/static/js/2.7da47713.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7da47713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae306707f3a1edab1127",
+    "url": "/static/js/main.cd0a6261.chunk.js"
+  },
+  {
+    "revision": "ebddc72d9a1f8ab6b643",
+    "url": "/static/js/runtime-main.1e83afd1.js"
+  }
+]);
